deb333ed0bd959fc3ffc2083b58b7e35c211a7d2
Ian Firdaus <ianfirdaus31@gmail.com>
2025-01-27 11:23:23 +0700

    Add typeorm module config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from 'src/lesson/lesson.entity';
 import { LessonModule } from 'src/lesson/lesson.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [
+        Lesson
+      ]
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver
